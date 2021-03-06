92204dc1614d30b7c1bd6456f2b42c062e378d8f
myelsoria <myrell.05@gmail.com>
2021-03-06 19:56:42 +0800

    updated style for Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Container, Button } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-      <Container>
-        <span>Lorem ipsum dolor sitamet,</span>
-        <span>consecteur adipiscing</span>
-        <span>Get aboard now speak with us!</span>
-        <Button type='button' variant='primary'>
+    <div className='flex-grow-1 text-center px-4'>
+      <div
+        className='d-flex flex-column justify-content-center align-items-center'
+        style={{ minHeight: '70vh' }}
+      >
+        <h2>Lorem ipsum dolor sitamet,</h2>
+        <h2>consecteur adipiscing</h2>
+        <h6>Get aboard now speak with us!</h6>
+        <Button type='button' variant='primary' className='my-1'>
           Request a pass
         </Button>
-      </Container>
+      </div>
     </div>
   )
 }
