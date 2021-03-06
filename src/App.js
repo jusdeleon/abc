196e34388b80e009eb920dba4f8875dd18c8a80d
myelsoria <myrell.05@gmail.com>
2021-03-06 18:28:00 +0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Header />
+        <Footer />
       </Router>
     </div>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h4>Start your day with ☕️</h4>
+            <h5>ABC Group. All rights reserved.</h5>
+        </footer>
+    )
+}
+
+export default Footer
